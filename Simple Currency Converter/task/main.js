4af32e0dd7ec41ec0bc5b5870d4a0124aed9a515
denys <denamyte@gmail.com>
2023-11-09 20:37:33 +0200

    2023-11-09: Stage 1/4

--- Simple Currency Converter/task/main.js
+++ Simple Currency Converter/task/main.js
@@ -1,1 +1,20 @@
-//Write your code here
+const welcome = () => console.log("Welcome to Currency Converter!")
+
+const printUsdToCurrency = (value, currency) => console.log(`1 USD equals ${value} ${currency}`)
+
+const map = {
+    'USD': 1,
+    'JPY': 113.5,
+    'EUR': 0.89,
+    'RUB': 74.36,
+    'GBP': 0.75,
+}
+
+function run() {
+    welcome()
+    for (curr in map) {
+        printUsdToCurrency(map[curr], curr)
+    }
+}
+
+run()
