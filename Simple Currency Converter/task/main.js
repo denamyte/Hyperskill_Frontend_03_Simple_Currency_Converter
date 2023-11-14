bf00afc0dc3889bc425ee73ab48a383e1026d4b1
denys <denamyte@gmail.com>
2023-11-14 13:19:31 +0200

    2023-11-13: Stage 2/4

--- Simple Currency Converter/task/main.js
+++ Simple Currency Converter/task/main.js
@@ -1,20 +1,72 @@
-const welcome = () => console.log("Welcome to Currency Converter!")
+const input = require("sync-input");
 
-const printUsdToCurrency = (value, currency) => console.log(`1 USD equals ${value} ${currency}`)
-
-const map = {
+const CUR_MAP = {
     'USD': 1,
     'JPY': 113.5,
     'EUR': 0.89,
     'RUB': 74.36,
     'GBP': 0.75,
-}
+};
 
-function run() {
-    welcome()
-    for (curr in map) {
-        printUsdToCurrency(map[curr], curr)
+const printUsdToCurrency = (value, currency) => console.log(`1 USD equals ${value} ${currency}`);
+
+const intro = () => {
+    console.log("Welcome to Currency Converter!");
+    for (let cur in CUR_MAP) {
+        printUsdToCurrency(CUR_MAP[cur], cur);
     }
+    console.log("I can convert USD to these currencies: JPY, EUR, RUB, USD, GBP");
+    console.log("Type the currency you wish to convert: USD");
 }
 
-run()
+const inputToCur = () => input("To: ").toUpperCase()
+
+const inputAmount = () => Number(input("Amount: "))
+
+const check = {
+    check: (res, error) => {
+        if (!res) console.log(error);
+        return res;
+    },
+
+    isNumber(num) {
+        return this.check(
+            Number.isFinite(num),
+            "The amount has to be a number"
+        );
+    },
+
+    isLT1(num) {
+        if (!this.isNumber(num)) return false;
+        return this.check(
+            num >= 1,
+            "The amount cannot be less than 1"
+        )
+    },
+
+    isCurrency(curList, cur) {
+        return this.check(
+            curList.includes(cur),
+            "Unknown currency"
+        )
+    },
+}
+
+function convert(fromCur, toCur, amount) {
+    return (amount / CUR_MAP[fromCur] * CUR_MAP[toCur]).toFixed(4)
+}
+
+function main() {
+    intro();
+    const fromCur = 'USD'
+    const toCur = inputToCur();
+    if (!check.isCurrency(Object.keys(CUR_MAP), toCur))
+        return;
+    const amount = inputAmount();
+    if (!check.isLT1(amount))
+        return;
+    const resAmount = convert(fromCur, toCur, amount)
+    console.log(`Result: ${amount} ${fromCur} equals ${resAmount} ${toCur}`)
+}
+
+main()
